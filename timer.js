5399386b6ea40eb78ad44b5f996116849d54c838
Ben-87 <137182762+Ben-87@users.noreply.github.com>
2026-01-25 14:34:06 -0500

    added timer

---
+++ timer.js
@@ -0,0 +1,67 @@
+let startBtn = document.getElementById('start');
+let pauseBtn = document.getElementById('pause');
+let finishBtn = document.getElementById('finish');
+
+let seconds = 0
+let minutes = 0
+let hours = 0
+
+//waits x milliseconds
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let timer = false;
+let running = false
+
+
+startBtn.addEventListener('click', function () {
+  timer = true;
+  if (!running) {stopwatch();}
+});
+pauseBtn.addEventListener('click', function () {
+  timer = false;
+});
+finishBtn.addEventListener('click', function () {
+  timer = false;
+});
+
+//stopwatch function
+async function stopwatch () {
+  running = true;
+  while (running) {
+    if (timer) {
+      await wait(1000);
+      seconds += 1;
+
+      minutes = Math.floor(seconds/60);
+      seconds = seconds % 60; 
+
+      hours = Math.floor(minutes/60);
+      minutes = minutes % 60;
+
+      updateDisplay()
+    }
+    else {
+      await wait(100)
+    }
+  }
+}
+
+function updateDisplay () {
+  //add leading 0
+  if (hours < 10) {
+    hrString = '0' + String(hours);
+  }
+  if (minutes < 10) {
+    minString = '0' + String(minutes);
+  }
+  if (seconds < 10) {
+    secString = '0' + String(seconds);
+  }
+
+  //write to html
+  document.getElementById('hr').innerHTML = hrString
+  document.getElementById('min').innerHTML = minString
+  document.getElementById('sec').innerHTML = secString
+}
